e85a7311b620b9ed6ac27febdda850e89d136c43
dcesaro <dcesaro@RMO01.facu.psi>
2024-06-21 18:52:30 -0300

    Se agrega manejo de crédito

--- test/model/CelularTest.java
+++ test/model/CelularTest.java
@@ -11,21 +11,35 @@ class CelularTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		
 		this.celular = new Celular(12345678);
-		
 	}
 
 	@Test
 	void testConstructor() {
-		
 		assertNotNull(celular);
 	}
 	
 	@Test
 	void testGetNumero() {
-		
 		assertEquals(12345678, celular.getNumero());
+	}
+	
+	@Test
+	void testGetCredito() {
+		assertEquals(0.0, celular.getCredito());
+	}
+	
+	@Test
+	void testCargarCredito() {
+		celular.cargarCredito(1000.00);
+		assertEquals(1000.00, celular.getCredito());
+	}
+	
+	@Test
+	void testConsumirCredito() {
+		celular.cargarCredito(1000.00);
+		celular.consumirCredito(500.00);
+		assertEquals(500.00, celular.getCredito());
 	}
 
 }
